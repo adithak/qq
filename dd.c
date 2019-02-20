f63173283bfab4742b51ab721cfd7374e5a8b0f4
adithak <adithak59@gmail.com>
2019-02-20 13:49:19 +0530

    Create dd.c

---
+++ dd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int m,s=0,t,b;
+scanf("%d",&m);
+b=m;
+while(m!=0)
+{
+	t=t*10;
+	t=t+(n%10);
+	m=n/10;
+}
+if(t==b)
+{
+	printf("Yes");
+}
+else
+{
+	printf("No");
+}
+return 0;
+}
