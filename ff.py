7dcf6f07ff331a8de3b5d26e6eeeede73dad9043
adithak <adithak59@gmail.com>
2019-02-20 13:56:17 +0530

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,11 @@
+q = []
+w=[]
+m=int(input("enter:"))
+for i in range(1,m+1):
+  b=int(input("enter"))
+  q.append(b)
+for j in range(1,m+1):
+  c=int(input("enter"))
+  w.append(c)
+print(max(q))
+print(max(w))
